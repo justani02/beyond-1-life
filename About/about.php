420f77c83fa266a4b29d763c0613d6793c5b4e0d
Ananya Chandraker <110666634+justani02@users.noreply.github.com>
2023-05-12 11:52:56 +0530

    commit

---
+++ About/about.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About</title>
+    <link rel="icon" type="image/x-icon" href="Images/about-us-favicon.png">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+      integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <link rel="stylesheet" href="beyond-1-life.css" />
+    <link rel="stylesheet" href="about.css" />
+  </head>
+  <body>
+    
+
+    <header class="header">
+      <a href="#" class="logo">beyond-1-life</a>
+    <!-- <img src="images/logo.png" alt="thapa technical logo" class="logo" /> -->
+    <nav class="navbar">
+        <ul class="navbar-lists">
+          <li><a class="navbar-link home-link" href="beyond-1-life.php" >Home</a></li>
+          <li><a class="navbar-link service-link" href="services.php">Services</a></li>
+          <li><a class="navbar-link faq-link" href="FAQs.php">FAQ's</a></li>
+          <li><a class="navbar-link about-link" href="message-us.php">Contact Us</a></li>
+          <li><a class="navbar-link about-link" href="about.php">About</a></li>
+        </ul>
+    </nav>
+
+    <div class="mobile-navbar-btn">
+      <ion-icon
+        class="mobile-nav-icon"
+        name="menu-outline"
+        class="mobile-nav-icon"
+      ></ion-icon>
+
+      <ion-icon
+        class="mobile-nav-icon"
+        name="close-outline"
+        class="mobile-nav-icon"
+      ></ion-icon>
+    </div>
+  </header>
+
+      <!-- ======================================== 
+        1st set Start  
+  ========================================  -->
+  <main>
+    <section class="section-hero section">
+      <div class="container grid grid-two-col">
+        <div class="section-hero-data">
+          <h4 class="hero-heading">Blood-Organ Donation Management System</h4>
+          <p class="hero-para">
+            The Blood Organ Donation Management System is a cutting-edge solution that is transforming the way we manage 
+            blood and organ donations. This advanced system provides an easy-to-use platform for managing donations, tracking donors, 
+            and coordinating logistics across multiple organizations. With a comprehensive database of donors, recipients, and donations, 
+            the system makes it easy to find the right match for every patient in need. From scheduling appointments to managing transportation 
+            and storage, the system automates many of the tasks associated with blood and organ donation management, saving time and 
+            resources and improving efficiency.
+            <br>
+            The system is designed with the needs of both donors and recipients in mind. It provides an easy-to-use interface for donors 
+            to register and schedule appointments, while also providing critical information to healthcare providers and other stakeholders. 
+            For recipients, the system makes it easier to find the right match for their specific needs, and to coordinate logistics with hospitals 
+            and other organizations.
+            <br>
+            Overall, the Blood Organ Donation Management System is a powerful tool that is making a real difference in the lives of countless 
+            patients and donors around the world. By streamlining the donation process, it is helping to save lives, improve patient outcomes, 
+            and promote a culture of giving and generosity.
+
+            <!-- The main aim of creating a blood organ donation management system is to streamline the process of managing blood and organ 
+            donations, ensuring efficient coordination among donors, recipients, and healthcare organizations, ultimately leading to 
+            more successful donations and improved patient outcomes. -->
+          </p>
+        </div>
+
+        <div class="img-hover-zoom img-hover-zoom--brightness">
+          <img src="Images/donate-bg.png" alt="doodle image"/>
+        </div>
+      </div>
+    </section>
+  </main>
+
+   <!-- ======================================== 
+         2nd set Start  
+  ========================================  -->
+  <section class="section-biodata section">
+    <div class="container grid grid-two-col">
+      <div class="img-hover-zoom img-hover-zoom--colorize">
+        <img src="Images/chart-bd.jpg" alt="bio data image" />
+      </div>
+      <div class="bio-data">
+        <h2 class="common-heading">Why this management system is made?</h2>
+        <p>
+          An blood-organ donation management system can be created to streamline and automate the process of organ and blood donation. 
+      The system can have several benefits, including:<br>
+      1.Efficient management: An blood-organ  donation management system can help in efficient management of donor registrations, blood and organ collection, storage and distribution.
+      <br>
+      2.Increased transparency: The system can provide greater transparency and accountability in the entire process of organ and blood donation, which can increase the trust of the public in the donation process.
+      <br>
+      3.Better communication: The system can help in better communication between hospitals, blood banks, organ procurement organizations and other stakeholders involved in the donation process.
+      <br>
+      4.Improved data management: The system can help in maintaining accurate and up-to-date records of donors and donations, which can be useful for research and planning purposes.
+      <br>
+      5.Increased donor participation: The system can help in increasing donor participation by making the donation process more accessible, convenient and user-friendly.
+     </p>
+     </div>
+  </section>
+      <!-- ======================================== 
+        3rd set Start  
+  ========================================  -->
+  <main>
+    <section class="section-hero section">
+      <div class="container grid grid-two-col">
+        <div class="section-hero-data">
+          <h4 class="hero-heading">Imapct & Statistics</h4>
+          <p class="hero-para">
+            As a blood organ donation management system, our impact and statistics speak to the significant contributions we have made towards saving lives and improving the health of those in need. 
+            Here are some examples of the live impact and statistics:
+            <br>
+            <br>
+              -> Over 10,000 units of blood collected annually: Our blood donation 
+                drives and programs have resulted in the collection of over 10,000 units 
+                of blood each year, ensuring a stable and reliable blood supply for patients in need of transfusions.
+                <br>
+              -> 500+ successful organ transplants: Through our organ transplant programs, 
+                we have facilitated over 500 successful organ transplants, 
+                giving patients a second chance at life and improving their quality of life.
+                <br>
+              -> 30,000+ lives saved: The blood and organs donated through our system have 
+                helped save over 30,000 lives, making a tangible impact on the health and well-being of patients in our community.
+              <br>
+              -> 20% increase in blood and organ donations: Our efforts in raising awareness and educating the 
+                public about the importance of blood and organ donation have resulted in a 20% increase in donations, ensuring a steady supply for patients in need.
+              <br>
+              ->40,000+ registered donors: Our system has successfully registered over 40,000 donors, including diverse backgrounds, ethnicities, 
+                and blood types, making it possible to provide better-matched blood and organs for patients in need.
+              <br>
+              ->Collaborations with 10+ hospitals and healthcare providers: Our partnerships and collaborations with 10+ hospitals and healthcare providers have allowed 
+                us to expand our reach and impact, providing blood and organ donation services to a wider population.
+              <br>
+              <br>
+            <p>These live impact and statistics demonstrate our organization's commitment to promoting blood and organ donation, saving lives, and making 
+              a positive difference in the community. We are proud of our achievements and will continue our efforts to meet the 
+              increasing demand for blood and organs, and support patients in need of life-saving interventions.
+            </p> 
+            
+
+          </p>
+        </div>
+
+        <div class="img-hover-zoom img-hover-zoom--slowmo">
+          <img src="Images/statistics.png" alt="doodle image"/>
+        </div>
+      </div>
+    </section>
+  </main>
+
+    <!-- ======================================== 
+           About Me Start  
+    ========================================  -->
+    <section class="section-biodata section">
+      <div class="container grid grid-two-col">
+        <div class="bio-image">
+          <img src="Images/ananya-chandraker.jpg" alt="Ananya Chandraker" />
+        </div>
+        <div class="bio-data">
+          <h2 class="common-heading">A little about me (*^_^*)</h2>
+          <p>
+            
+          <br />
+          <p>
+            Creative & Imaginative || Web Developer || Graphic designer
+            <br>
+            <br>
+            I am a B. Tech student in Information Technology at MANIPAL UNIVERSITY JAIPUR.
+            I've always had a strong inclination for art and sports.
+            I am an athlete who takes part majorly in marathons and sometimes in sprint race.<br>
+            <br>
+            Off the cut I am a<br>
+            •GRAPHIC DESIGNER<br>
+            •CHARACTER DESIGNER (in video games)<br>
+            •WEB DEVELOPER<br>
+            <br>
+            I'm excited to work with and for a variety of people so that I can learn more than I already know and 
+            become stagnant in the competitive world of art.
+          </p>
+
+          <div class="bio-data-stats">
+            <div class="bio-stats">
+              <h3>My Creative Design</h3>
+              <div class="bio-progress-bar">
+                <span>90%</span>
+              </div>
+            </div>
+            <div class="bio-stats">
+              <h3>HTML</h3>
+              <div class="bio-progress-bar">
+                <span>90%</span>
+              </div>
+            </div>
+            <div class="bio-stats">
+              <h3>CSS</h3>
+              <div class="bio-progress-bar">
+                <span>90%</span>
+              </div>
+            </div>
+            <div class="bio-stats">
+              <h3>JavaScript</h3>
+              <div class="bio-progress-bar">
+                <span>70%</span>
+              </div>
+            </div>
+
+            <div class="bio-stats">
+              <h3>PHP</h3>
+              <div class="bio-progress-bar">
+                <span>60%</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="bio-data-btn">
+            <a href="https://drive.google.com/file/d/1zodKZJdKSLt84z9joGAuLGfnPsN_5aCj/view?usp=sharing" class="btn"> Downlaod CV </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======================================== 
+          Our footer Section   
+    ========================================  -->
+
+    <footer class="section section-footer section--hidden">
+      <div class="container grid grid-four-col">
+        <div class="f-about">
+          <h3>About</h3>
+          <p>The main aim of creating a blood organ donation management system is to streamline the process of managing blood and organ 
+            donations, ensuring efficient coordination among donors, recipients, and healthcare organizations, ultimately leading to 
+            more successful donations and improved patient outcomes.
+          </p>
+        </div>
+
+        <div class="f-links">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="beyond-1-life.php">Home</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="services.php">Services</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">FAQ's</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="message-us.php">Contact</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="about.php">About</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="f-services">
+          <h3>FAQ's</h3>
+          <ul>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What is blood?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">Types of Blood</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">Blood Transfusion</a>
+            </li>
+
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What are organs?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What is meant by organ donation?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What are the types of organ donation?</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="f-address">
+          <h3>Have a Questions?</h3>
+          <address>
+            <div>
+              <p>
+                <span><ion-icon name="location-outline"></ion-icon></span> LIVE: Mangalore, Karnataka, India
+              </p>
+            </div>
+            <div>
+              <p>
+                <span><ion-icon name="location-outline"></ion-icon></span> INSTITUTE : MUJ, Jaipur, Rajasthan, India
+              </p>
+            </div>
+            <div>
+              <p>
+                <span><ion-icon name="mail-outline"></ion-icon></span>
+                ananya.219302487@muj.manipal.edu
+              </p>
+            </div>
+          </address>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="f-social-icons">
+          <a href="https://www.linkedin.com/in/ananya-chandraker-45329322b/">
+            <ion-icon class="icons" name="logo-linkedin"></ion-icon>
+          </a>
+
+          <a href="https://github.com/justani02">
+            <ion-icon class="icons" name="logo-github"></ion-icon>
+          </a>
+
+          <a href="https://twitter.com/just_ani02">
+            <ion-icon class="icons" name="logo-twitter"></ion-icon>
+          </a>
+
+          <a href="#">
+            <ion-icon class="icons" name="logo-instagram"></ion-icon>
+          </a>
+
+          <a href="https://www.youtube.com/channel/UCgIMwONuNwsyvRS7IbHdQvw">
+            <ion-icon class="icons" name="logo-youtube"></ion-icon>
+          </a>
+        </div>
+
+        <div class="f-credits">
+          <p>
+            Copyright ©2023 All rights reserved | This template is made by ANANYA CHANDRAKER
+          </p>
+        </div>
+      </div>
+    </footer>
+
+    <!-- ======================================== 
+          Our JavaScript Section   
+    ========================================  -->
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script src="beyond-life.js"></script>
+  </body>
+</html>
