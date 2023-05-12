809775dd52659020769921551eb220ac8ad81358
Ananya Chandraker <110666634+justani02@users.noreply.github.com>
2023-05-12 11:51:13 +0530

    Commit

---
+++ Services/services.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services</title>
+    <link rel="icon" type="image/x-icon" href="Images/faq.png">
+
+    <link rel="stylesheet" href="beyond-1-life.css">
+    <link rel="stylesheet" href="services.css">
+
+
+</head>
+<body>
+  <header class="header">
+    <a href="#" class="logo">beyond-1-life</a>
+    <!-- <img
+      src="/index/Images/LOGO.png"
+      alt="beyond-1-life logo"
+      class="logo"
+    /> -->
+    <<nav class="navbar">
+        <ul class="navbar-lists">
+          <li><a class="navbar-link home-link" href="beyond-1-life.php" >Home</a></li>
+          <li><a class="navbar-link service-link" href="services.php">Services</a></li>
+          <li><a class="navbar-link faq-link" href="FAQs.php">FAQ's</a></li>
+          <li><a class="navbar-link about-link" href="message-us.php">Contact Us</a></li>
+          <li><a class="navbar-link about-link" href="about.php">About</a></li>
+        </ul>
+    </nav>
+
+    <div class="mobile-navbar-btn">
+      <ion-icon
+        class="mobile-nav-icon"
+        name="menu-outline"
+        class="mobile-nav-icon"
+      ></ion-icon>
+
+      <ion-icon
+        class="mobile-nav-icon"
+        name="close-outline"
+        class="mobile-nav-icon"
+      ></ion-icon>
+    </div>
+  </header>
+<!-- ======================================== 
+          Our Service Section Start  
+    ========================================  -->
+
+    <!-- ======================================== 
+                    1st set  
+    ========================================  -->
+
+
+    <section class="section-biodata section">
+      <div class="container grid grid-two-col">
+        <div class="img-hover-zoom img-hover-zoom--colorize">
+          <img src="Images/organs.png" alt="bio data image" />
+        </div>
+        <div class="bio-data">
+          <h2 class="common-heading">Organ Donation</h2>
+          <p>
+            Organ donation is a life-changing gift that has the power to save and improve 
+            the lives of many people. When someone donates an organ, it can help someone else 
+            to live longer, healthier, and happier. There are many types of organs that can be donated, 
+            including the heart, lungs, kidneys, liver, and pancreas. The process of becoming an organ 
+            donor is simple and can be done by registering online or at your local DMV. By becoming an 
+            organ donor, you have the power to make a real difference in someone else's life. 
+            It's estimated that one organ donor can save up to eight lives, and improve the lives of 
+            up to 75 people through tissue donation. Organ donation is a selfless act of kindness that 
+            can provide hope and healing to those in need. It's a powerful way to leave a lasting legacy 
+            and make a positive impact on the world.
+          </p>
+       </div>
+    </section>
+
+    <!-- ======================================== 
+             2nd set 
+  ========================================  -->
+  <main>
+    <section class="section-hero section">
+      <div class="container grid grid-two-col">
+        <div class="section-hero-data">
+          <h4 class="hero-heading">Blood Donation</h4>
+          <p class="hero-para">
+            Blood donation is a simple and selfless act that can save lives. Each year, 
+            millions of people require blood transfusions due to accidents, surgeries, and other 
+            medical emergencies. By donating blood, you can make a real difference in the lives of those in need. 
+            Not only does donating blood help to save lives, but it also has health benefits for the donor. 
+            It stimulates the production of new blood cells, reduces the risk of certain cancers, and can even 
+            help lower the risk of heart disease. Donating blood is a safe and easy process that typically takes 
+            less than an hour, and anyone in good health who meets the eligibility requirements can donate. 
+            By donating blood, you become a part of a life-saving community and help to ensure that hospitals and 
+            medical centers have a steady supply of blood to help those in need.
+          </p>
+        </div>
+
+        <div class="img-hover-zoom img-hover-zoom--brightness">
+          <img src="Images/blood-compositon-removebg-preview.png" alt="doodle image"/>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  
+    <!-- ======================================== 
+          Our footer Section   
+    ========================================  -->
+
+    <footer class="section section-footer section--hidden">
+      <div class="container grid grid-four-col">
+        <div class="f-about">
+          <h3>About</h3>
+          <p>The main aim of creating a blood organ donation management system is to streamline the process of managing blood and organ 
+            donations, ensuring efficient coordination among donors, recipients, and healthcare organizations, ultimately leading to 
+            more successful donations and improved patient outcomes.
+          </p>
+        </div>
+
+        <div class="f-links">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="beyond-1-life.php">Home</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="services.php">Services</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">FAQ's</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="message-us.php">Contact</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="about.php">About</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="f-services">
+          <h3>FAQ's</h3>
+          <ul>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What is blood?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">Types of Blood</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">Blood Transfusion</a>
+            </li>
+
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What are organs?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What is meant by organ donation?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What are the types of organ donation?</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="f-address">
+          <h3>Have a Questions?</h3>
+          <address>
+            <div>
+              <p>
+                <span><ion-icon name="location-outline"></ion-icon></span> LIVE: Mangalore, Karnataka, India
+              </p>
+            </div>
+            <div>
+              <p>
+                <span><ion-icon name="location-outline"></ion-icon></span> INSTITUTE : MUJ, Jaipur, Rajasthan, India
+              </p>
+            </div>
+            <div>
+              <p>
+                <span><ion-icon name="mail-outline"></ion-icon></span>
+                ananya.219302487@muj.manipal.edu
+              </p>
+            </div>
+          </address>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="f-social-icons">
+          <a href="https://www.linkedin.com/in/ananya-chandraker-45329322b/">
+            <ion-icon class="icons" name="logo-linkedin"></ion-icon>
+          </a>
+
+          <a href="https://github.com/justani02">
+            <ion-icon class="icons" name="logo-github"></ion-icon>
+          </a>
+
+          <a href="https://twitter.com/just_ani02">
+            <ion-icon class="icons" name="logo-twitter"></ion-icon>
+          </a>
+
+          <a href="#">
+            <ion-icon class="icons" name="logo-instagram"></ion-icon>
+          </a>
+
+          <a href="https://www.youtube.com/channel/UCgIMwONuNwsyvRS7IbHdQvw">
+            <ion-icon class="icons" name="logo-youtube"></ion-icon>
+          </a>
+        </div>
+
+        <div class="f-credits">
+          <p>
+            Copyright ©2023 All rights reserved | This template is made by ANANYA CHANDRAKER
+          </p>
+        </div>
+      </div>
+    </footer>
+
+    <!-- ======================================== 
+          Our JavaScript Section   
+    ========================================  -->
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script src="index.js"></script>
+  </body>
+</html>
+
+
+</body>
+
+<script src="beyond-life.js"></script>
+</html>
