d31a8d5377ea0548d1d63a19cec98c2e95873046
Ananya Chandraker <110666634+justani02@users.noreply.github.com>
2023-05-12 11:57:43 +0530

    Add files via upload

---
+++ FAQ's/FAQs.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ's</title>
+    <link rel="icon" type="image/x-icon" href="Images/faq.png">
+
+    <link rel="stylesheet" href="beyond-1-life.css">
+
+
+</head>
+<body>
+  <header class="header">
+    <a href="#" class="logo">beyond-1-life</a>
+    <!-- <img
+      src="/index/Images/LOGO.png"
+      alt="beyond-1-life logo"
+      class="logo"
+    /> -->
+    <nav class="navbar">
+        <ul class="navbar-lists">
+          <li><a class="navbar-link home-link" href="beyond-1-life.php" >Home</a></li>
+          <li><a class="navbar-link service-link" href="services.php">Services</a></li>
+          <li><a class="navbar-link faq-link" href="FAQs.php">FAQ's</a></li>
+          <li><a class="navbar-link about-link" href="message-us.php">Contact Us</a></li>
+          <li><a class="navbar-link about-link" href="about.php">About</a></li>
+        </ul>
+    </nav>
+
+    <div class="mobile-navbar-btn">
+      <ion-icon
+        class="mobile-nav-icon"
+        name="menu-outline"
+        class="mobile-nav-icon"
+      ></ion-icon>
+
+      <ion-icon
+        class="mobile-nav-icon"
+        name="close-outline"
+        class="mobile-nav-icon"
+      ></ion-icon>
+    </div>
+  </header>
+
+    <!-- ======================================== 
+          FAQ's Start  
+    ========================================  -->
+    <section class="section-services section ">
+      <div class="container">
+        <h2 class="common-heading">FAQ's</h2>
+      </div>
+
+      <div class="container grid grid-three-col">
+        
+
+        <div class="service-box">
+          <img src="Images/blood-drop-removebg-preview.png" class="service-icon">
+          <h3>What is blood?</h3>
+          <p>
+            Your blood is made up of liquid and solids. The liquid part, called plasma, is made of water, salts, and protein. Over half of your blood is plasma.
+            The solid part of your blood contains red blood cells, white blood cells, and platelets. Red blood cells (RBC) deliver oxygen from your lungs to your tissues and organs.
+          </p>
+        </div>
+
+        <div class="service-box">
+          <img src="Images/blood-drop-removebg-preview.png" class="service-icon">
+          <h3>Types of Blood</h3>
+          <p>The 4 different blood groups in the ABO system are A, B, AB and O. A person's blood group is determined by a pair of genes-one gene inherited from each parent.
+            Each blood group is identified by its own set of molecules (called antigens), which are located on the surface of red blood cells.</p>
+        </div>
+
+
+        <div class="service-box">
+          <img src="Images/blood-drop-removebg-preview.png" class="service-icon">
+          <h3>Blood Transfusion</h3>
+          <p class="mb-0">Blood transfusion is needed for:<br>
+              1. Women with complications of pregnancy, such as ectopic pregnancies and
+              haemorrhage before, during or after childbirth.<br>
+              2.Children with severe anaemia often resulting from malaria or malnutrition.<br>
+              3.People with severe trauma following man-made and natural disasters.<br>
+              4.Many complex medical and surgical procedures and cancer patients.<br>
+            </p>
+        </div>
+
+        <div class="service-box">
+          <img src="Images/002-liver.png" class="service-icon">
+          <h3>What are organs?</h3>
+          <p>
+            In biology, an organ (from the Latin "organum" meaning an instrument or tool) is a collection of tissues that structurally 
+            form a functional unit specialized to perform a particular function. Your heart, kidneys, and lungs are examples of organs
+          </p>
+        </div>
+        <div class="service-box">
+          <img src="Images/002-liver.png" class="service-icon">
+          <h3>What is meant by organ donation?</h3>
+          <p>Organ donation is when you decide to give an organ to save or transform the life of someone else. You can donate some organs while you are alive,
+            and this is called living organ donation.
+            However, most organ and tissue donations come from people who have died.
+          </p>
+        </div>
+
+        <div class="service-box">
+          <img src="Images/002-liver.png" class="service-icon">
+          <h3>What are the types of organ donation?</h3>
+          <p>
+            By registering to become an organ donor you have the option to donate organs such as your heart, lungs, liver, kidneys, pancreas and small bowel.
+            All of these forms of donation can greatly enhance or even save the life of someone in need.
+        </p>
+        </div>
+      </div>
+    </section>
+    <!-- ======================================== 
+          Our footer Section   
+    ========================================  -->
+
+    <footer class="section section-footer section--hidden">
+      <div class="container grid grid-four-col">
+        <div class="f-about">
+          <h3>About</h3>
+          <p>The main aim of creating a blood organ donation management system is to streamline the process of managing blood and organ 
+            donations, ensuring efficient coordination among donors, recipients, and healthcare organizations, ultimately leading to 
+            more successful donations and improved patient outcomes.
+          </p>
+        </div>
+
+        <div class="f-links">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="beyond-1-life.php">Home</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="services.php">Services</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">FAQ's</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="message-us.php">Contact</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="about.php">About</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="f-services">
+          <h3>FAQ's</h3>
+          <ul>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What is blood?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">Types of Blood</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">Blood Transfusion</a>
+            </li>
+
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What are organs?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What is meant by organ donation?</a>
+            </li>
+            <li>
+              <span><ion-icon name="arrow-forward-outline"></ion-icon></span
+              ><a href="FAQs.php">What are the types of organ donation?</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="f-address">
+          <h3>Have a Questions?</h3>
+          <address>
+            <div>
+              <p>
+                <span><ion-icon name="location-outline"></ion-icon></span> LIVE: Mangalore, Karnataka, India
+              </p>
+            </div>
+            <div>
+              <p>
+                <span><ion-icon name="location-outline"></ion-icon></span> INSTITUTE : MUJ, Jaipur, Rajasthan, India
+              </p>
+            </div>
+            <div>
+              <p>
+                <span><ion-icon name="mail-outline"></ion-icon></span>
+                ananya.219302487@muj.manipal.edu
+              </p>
+            </div>
+          </address>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="f-social-icons">
+          <a href="https://www.linkedin.com/in/ananya-chandraker-45329322b/">
+            <ion-icon class="icons" name="logo-linkedin"></ion-icon>
+          </a>
+
+          <a href="https://github.com/justani02">
+            <ion-icon class="icons" name="logo-github"></ion-icon>
+          </a>
+
+          <a href="https://twitter.com/just_ani02">
+            <ion-icon class="icons" name="logo-twitter"></ion-icon>
+          </a>
+
+          <a href="#">
+            <ion-icon class="icons" name="logo-instagram"></ion-icon>
+          </a>
+
+          <a href="https://www.youtube.com/channel/UCgIMwONuNwsyvRS7IbHdQvw">
+            <ion-icon class="icons" name="logo-youtube"></ion-icon>
+          </a>
+        </div>
+
+        <div class="f-credits">
+          <p>
+            Copyright ©2023 All rights reserved | This template is made by ANANYA CHANDRAKER
+          </p>
+        </div>
+      </div>
+    </footer>
+
+    <!-- ======================================== 
+          Our JavaScript Section   
+    ========================================  -->
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script src="index.js"></script>
+  </body>
+</html>
+
+
+</body>
+
+<script src="beyond-life.js"></script>
+</html>
